2b0c3107dff7d86409c95b17f609df19c36170b2
spb31 <93053933+spb31@users.noreply.github.com>
2022-03-05 16:06:03 +0000

    Added duck resizer

---
+++ WordScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordScript : MonoBehaviour
+{
+    public GameObject duck;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void IncreaseSize()
+    {
+        Vector3 currentScaleVector = duck.transform.localScale;
+        currentScaleVector.x = currentScaleVector.x + 1;
+        currentScaleVector.y = currentScaleVector.y + 1;
+        currentScaleVector.z = currentScaleVector.z + 1;
+        duck.transform.localScale = currentScaleVector;
+    }
+}
